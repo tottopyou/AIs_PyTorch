bdfc76fff9dcf7e4d7f0ed646beb1cc7c4b906c9
tottopyou <rgrabovsky2006@gmail.com>
2024-04-30 15:29:48 +0200

    Computer vision

--- Computer Vision/Computer_vision.py
+++ Computer Vision/Computer_vision.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
+import random
 import torchvision
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-from helper_functions import accuracy_fn
+from helper_functions import accuracy_fn, make_predictions
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
@@ -82,7 +85,7 @@ class ComputerVision(nn.Module):
 
 model = ComputerVision(
     input_shape=1,
-    hidden_units=10,
+    hidden_units=20,
     output_shape=len(class_names))
 
 loss_fn = nn.CrossEntropyLoss()
@@ -90,7 +93,7 @@ optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
 
 torch.manual_seed(42)
 
-epochs = 3
+epochs = 10
 for epoch in tqdm(range(epochs)):
     print(f"Epoch: {epoch}\n---------")
     train_step(data_loader=train_dataloader,
@@ -104,3 +107,61 @@ for epoch in tqdm(range(epochs)):
         loss_fn=loss_fn,
         accuracy_fn=accuracy_fn,
     )
+
+
+test_samples = []
+test_labels = []
+for sample, label in random.sample(list(test_data), k=9):
+    test_samples.append(sample)
+    test_labels.append(label)
+
+pred_probs= make_predictions(model=model,
+                             data=test_samples)
+
+pred_classes = pred_probs.argmax(dim=1)
+
+plt.figure(figsize=(9, 9))
+plt.clf()
+nrows = 3
+ncols = 3
+for i, sample in enumerate(test_samples):
+
+    plt.subplot(nrows, ncols, i + 1)
+
+    plt.imshow(sample.squeeze(), cmap="gray")
+
+    pred_label = class_names[pred_classes[i]]
+
+    truth_label = class_names[test_labels[i]]
+
+    title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+
+    if pred_label == truth_label:
+        plt.title(title_text, fontsize=10, c="g")
+    else:
+        plt.title(title_text, fontsize=10, c="r")
+    plt.axis(False)
+
+plt.tight_layout()
+plt.show()
+
+
+y_preds = []
+model.eval()
+with torch.inference_mode():
+  for X, y in tqdm(test_dataloader, desc="Making predictions"):
+    X, y = X.to(device), y.to(device)
+    y_logit = model(X)
+    y_pred = torch.softmax(y_logit, dim=1).argmax(dim=1)
+    y_preds.append(y_pred.cpu())
+y_pred_tensor = torch.cat(y_preds)
+
+confmat = ConfusionMatrix(num_classes=len(class_names), task='multiclass')
+confmat_tensor = confmat(preds=y_pred_tensor,
+                         target=test_data.targets)
+fig, ax = plot_confusion_matrix(
+    conf_mat=confmat_tensor.numpy(),
+    class_names=class_names,
+    figsize=(10, 7)
+)
+plt.show()
--- Computer Vision/helper_functions.py
+++ Computer Vision/helper_functions.py
@@ -4,7 +4,26 @@ import numpy as np
 
 from torch import nn
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
+
+
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device = device):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+
+            sample = torch.unsqueeze(sample, dim=0).to(device)
+
+            pred_logit = model(sample)
+
+            pred_prob = torch.softmax(pred_logit.squeeze(),
+                                      dim=0) 
+            pred_probs.append(pred_prob.cpu())
+
+    return torch.stack(pred_probs)
