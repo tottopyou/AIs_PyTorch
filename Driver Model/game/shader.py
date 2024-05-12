159ad3123971d0edc276772b4cdbe9c433d340f1
tottopyou <rgrabovsky2006@gmail.com>
2024-05-12 16:44:12 +0200

    Driver model taking info from the game

--- Driver Model/game/shader.py
+++ Driver Model/game/shader.py
@@ -1,11 +1,7 @@
-# copy
 import pygame
 from pygame.locals import *
 
-
 class shader:
-    lights = []
-    
     def __init__(self, surf):
 
         self.size = surf.get_size()
@@ -16,7 +12,6 @@ class shader:
     def setup(self, color):
         self.bg = color
 
-
     def render(self, *args):
         self.fog.fill(self.bg)
 
@@ -25,16 +20,3 @@ class shader:
             l.render(self.fog, l.pos)
 
         self.surface.blit(self.fog, (0, 0), special_flags=BLEND_MULT)
-        
-
-        
-        
-
-
-
-### Shader Layer
-##fog = pygame.Surface((ww, wh))
-##fog_color = (3, 3, 3) # night fog color
-##
-##light_mask = pygame.image.load(path + "light.png")#"light.png")
-##light_rect = light_mask.get_rect()
